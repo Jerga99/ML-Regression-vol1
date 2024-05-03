361d614998150a6233f2b0b8a87441800f86e6e1
Filip Jerga <jerga99@gmail.com>
2024-05-03 08:15:22 +0100

    Plot the data

--- regression-app/src/App.js
+++ regression-app/src/App.js
@@ -1,10 +1,38 @@
 
 import './App.css';
+import Plot from "react-plotly.js";
+
+const studyHours = [1, 2, 3, 4, 5];
+const examScores = [55, 70, 80, 85, 90];
 
 function App() {
+
+  const data = [{
+    x: studyHours,
+    y: examScores,
+    mode: "markers",
+    type: "scatter",
+    marker: { color: "blue" }
+  }];
+
+  const layout = {
+    title: "Study hours vs Exam Scores",
+    xaxis: {
+      title: "Study hours",
+      autorange: true,
+    },
+    yaxis: {
+      title: "Exam scores",
+      autorange: true,
+    },
+  }
+
   return (
     <div className="App">
-      Hello World
+      <Plot
+        data={data}
+        layout={layout}
+      />
     </div>
   );
 }
