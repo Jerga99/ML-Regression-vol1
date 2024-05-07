416e2d32f6da1e448dc408a88f194855658bb8cd
Filip Jerga <jerga99@gmail.com>
2024-05-07 13:32:40 +0100

    Another multiplication

--- regression-app/src/models/mr-example.js
+++ regression-app/src/models/mr-example.js
@@ -10,12 +10,17 @@ const X = [
 ];
 
 const Y = [60,55,50,70,85]; // scores
+const YMatrix = math.matrix(Y).resize([5, 1]);
+
 const XWithIntercept = X.map(row => [1].concat(row));
 
 const XMatrix = math.matrix(XWithIntercept);
 const XTransposed = math.transpose(XMatrix);
 const XTransposedX = math.multiply(XTransposed, XMatrix);
+const XTransposedY = math.multiply(XTransposed, YMatrix);
 
-console.log(XTransposedX);
+console.log(XTransposedY);
+
+
 
 
