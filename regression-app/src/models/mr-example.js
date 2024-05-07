05892983c44ec7f12676f30bfeba1d1b0b1dbe6a
Filip Jerga <jerga99@gmail.com>
2024-05-07 09:30:09 +0100

    Organize matrices in code

--- regression-app/src/models/mr-example.js
+++ regression-app/src/models/mr-example.js
@@ -1,9 +1,21 @@
 
 
 
+const X = [
+  [1, 6], // [study hour, sleep hour]
+  [2, 2],
+  [3, 1],
+  [4, 5],
+  [5, 7]
+];
 
-const studyHours = [1,2,3,4,5];
-const sleepHours = [6,2,1,5,7];
-const score = [60,55,50,70,85];
+const Y = [60,55,50,70,85]; // scores
 
-console.log("Hello World!");
+const XWithIntercept = X.map(row => [1].concat(row));
+
+console.log(XWithIntercept);
+
+console.log("------------");
+console.log("------------");
+
+console.log(Y);
