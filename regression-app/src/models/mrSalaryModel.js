cf904d47a4540599bc241d0178385d8ad7b67355
Filip Jerga <jerga99@gmail.com>
2024-05-09 08:38:50 +0100

    Parse data from CSV

--- regression-app/src/models/mrSalaryModel.js
+++ regression-app/src/models/mrSalaryModel.js
@@ -1,8 +1,44 @@
 
+const fs = require("fs");
+const {parse} = require("csv-parse");
+
+const parseDataType = (data) => {
+
+  if (/^-?\d+$/.test(data)) {
+    return parseInt(data, 10);
+  }
+
+  else if (/^-?\d*\.\d+$/.test(data)) {
+    return parseFloat(data);
+  }
+
+  return data;
+}
+
+const readCSV = (filePath, keys) => {
+  return new Promise((resolve, reject) => {
+    let data = [];
+
+    fs.createReadStream(filePath)
+      .pipe(parse({columns: true, skip_empty_lines: true}))
+      .on("data", (urow) => {
+        const row = keys.map(key => {
+          return parseDataType(urow[key])
+        });
+
+        data.push(row);
+      })
+      .on("end", () => {
+        resolve(data);
+      })
+      .on("error", reject)
+  });
+}
 
 
-function computeModel(path) {
-  console.log(path)
+async function computeModel(path) {
+  const data = await readCSV(path, ["age", "experience", "income"]);
+  console.log(data);
 }
 
 computeModel("./public/age_exp_salary_dataset.csv");
