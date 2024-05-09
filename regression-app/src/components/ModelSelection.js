2329094d1a7ecc8347abb7fe0631476c5a2f64c0
Filip Jerga <jerga99@gmail.com>
2024-05-09 08:22:09 +0100

    Data preparation

--- regression-app/src/components/ModelSelection.js
+++ regression-app/src/components/ModelSelection.js
@@ -7,17 +7,25 @@ const model1 = {
   name: "Exam Score Prediction",
   description: "Predicts score based on study hours.",
   img: "https://images.unsplash.com/photo-1570616969692-54d6ba3d0397?q=80&w=3522&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-}
+};
 
 const model2 = {
   id: "2",
   type: "Multiple Linear Regression",
   name: "Exam Score Prediction",
   description: "Predicts score based on study hours and sleep hours.",
-  img: "https://images.unsplash.com/photo-1570616969692-54d6ba3d0397?q=80&w=3522&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  img: "https://images.unsplash.com/photo-1580582932707-520aed937b7b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2Nob29sfGVufDB8fDB8fHww"
+};
+
+const model3 = {
+  id: "3",
+  type: "Multiple Linear Regression",
+  name: "Salary Prediction",
+  description: "Predicts salary based on age and years of experience",
+  img: "https://plus.unsplash.com/premium_photo-1661281350976-59b9514e5364?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8b2ZmaWNlfGVufDB8fDB8fHww"
 }
 
-const models = [model1, model2];
+const models = [model1, model2, model3];
 
 
 function ModelSelection({onChange, activeId}) {
---
+++ regression-app/src/models/mrSalaryModel.js
@@ -0,0 +1,8 @@
+
+
+
+function computeModel(path) {
+  console.log(path)
+}
+
+computeModel("./public/age_exp_salary_dataset.csv");
