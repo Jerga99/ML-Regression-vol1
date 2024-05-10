94225bbdc9954215b5ab67a4a3d76bf6ca78c51e
Filip Jerga <jerga99@gmail.com>
2024-05-10 11:41:48 +0100

    Simplify car name

--- regression-app/src/models/mrCarModel.js
+++ regression-app/src/models/mrCarModel.js
@@ -3,10 +3,24 @@ const { readCSV } = require("./utils");
 
 const allCategories = ["symboling","CarName","fueltype","aspiration","doornumber","carbody","drivewheel","enginelocation","wheelbase","carlength","carwidth","carheight","curbweight","enginetype","cylindernumber","enginesize","fuelsystem","boreratio","stroke","compressionratio","horsepower","peakrpm","citympg","highwaympg","price"];
 
+const simplifyCarNames = (data) => {
+  data.forEach((item) => {
+    const brand = item.CarName.split(" ")[0].toLowerCase();
+    item.CarName = brand;
+  });
+
+  return data;
+}
+
+const processData = (data) => {
+  data = simplifyCarNames(data);
+  return data;
+}
+
 const computeModel = async (path) => {
   const data = await readCSV(path, allCategories, "dictionary");
-
-  console.log(data);
+  const processedData = processData(data);
+  console.log(processedData);
 }
 
 computeModel("./public/carprice_assignment.csv");
