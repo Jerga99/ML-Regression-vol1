c7072b9e6f4872e1b970fbc212fa14c8cf291d81
Filip Jerga <jerga99@gmail.com>
2024-05-11 12:24:01 +0100

    Fix typos in car names

--- regression-app/src/models/mrCarModel.js
+++ regression-app/src/models/mrCarModel.js
@@ -3,10 +3,27 @@ const { readCSV } = require("./utils");
 
 const allCategories = ["symboling","CarName","fueltype","aspiration","doornumber","carbody","drivewheel","enginelocation","wheelbase","carlength","carwidth","carheight","curbweight","enginetype","cylindernumber","enginesize","fuelsystem","boreratio","stroke","compressionratio","horsepower","peakrpm","citympg","highwaympg","price"];
 
+const displayUniqueValues = (data, category) => {
+  const set = new Set();
+  data.forEach(row => {
+    set.add(row[category]);
+  });
+
+  console.log(set);
+}
+
 const simplifyCarNames = (data) => {
+  const fixedBrands = {
+    "maxda": "mazda",
+    "porcshce": "porsche",
+    "toyouta": "toyota",
+    "vokswagen": "volkswagen",
+    "vw": "volkswagen"
+  };
+
   data.forEach((item) => {
     const brand = item.CarName.split(" ")[0].toLowerCase();
-    item.CarName = brand;
+    item.CarName = fixedBrands[brand] || brand;
   });
 
   return data;
@@ -20,7 +37,9 @@ const processData = (data) => {
 const computeModel = async (path) => {
   const data = await readCSV(path, allCategories, "dictionary");
   const processedData = processData(data);
-  console.log(processedData);
+
+  displayUniqueValues(data, "CarName");
+
 }
 
 computeModel("./public/carprice_assignment.csv");
